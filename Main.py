48c52bf2708d8063714faea287d9cd9755634012
Tyrell <Tyrell1052@gmail.com>
2018-02-15 21:56:31 -0500

    finished revised

--- Main.py
+++ Main.py
@@ -13,17 +13,20 @@ dblhours = float(input("How many hours did you work this week ?"))
 
 if dblhours > 40:
     overtimerate = 1.5 * DBLPAY
-    overtime = 1600+(dblhours - 40) * overtimerate
+    pay = 1600+(dblhours - 40) * overtimerate
     flag = 1
-
 else:
-    basepay = DBLPAY * dblhours
+    pay = DBLPAY * dblhours
     flag = 0
 
 # Output
 
 if(flag == 1):
-    print(overtime)
+    print("Gross pay with overtime",pay)
+    print("Overtime Hours",dblhours - 40)
 else:
-    (flag == 0)
-    print(basepay)
+    print("Gross pay without overtime",pay)
+
+print("Hours worked",dblhours)
+print("Pay",pay)
+print("Hours Overtime", dblhours - 40)
